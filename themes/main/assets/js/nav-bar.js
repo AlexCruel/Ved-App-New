6a1d26f314007378b57a963c20b0a5866f95e6db
Alex Karpenkov <sasha.karpenkov.34@gmail.com>
2023-10-22 12:30:37 +0300

    upd: header

--- themes/main/assets/js/nav-bar.js
+++
@@ -1,9 +0,0 @@
-$(window).resize(function () {
-    let windowSize = $(window).width();
-
-    if (windowSize <= 1200) {
-        $('ul').removeClass('gap-5');
-    } else {
-        $('ul').addClass('gap-5');
-    }
-});
